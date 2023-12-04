a39e1bcd7473d775be89f653d298311acaeee4f9
huubd <huub.dorgelo@gmail.com>
2023-12-04 15:14:09 +0100

    Day 2

---
+++ 2023/Day-2/day2-part1.py
@@ -0,0 +1,29 @@
+with open('input.txt') as f:
+    idSum = 0
+    for game in f:
+        gameId = game.split(':')[0]
+        game = game.split(':')[1].strip()
+        possible = True
+
+        gameRounds = game.split(';')
+
+        for gameRound in gameRounds:
+            for cube in gameRound.split(','):
+                cube = cube.strip()
+                color = cube.split(' ')[1]
+                count = int(cube.split(' ')[0])
+
+                if color == 'red' and count > 12:
+                    possible = False
+                    break
+                elif color == 'green' and count > 13:
+                    possible = False
+                    break
+                elif color == 'blue' and count > 14:
+                    possible = False
+                    break
+
+        if possible:
+            idSum += int(gameId.split(' ')[1])
+       
+    print(idSum) 
---
+++ 2023/Day-2/day2-part2.py
@@ -0,0 +1,26 @@
+with open('input.txt') as f:
+    result = 0
+    for game in f:
+        redCubes, greenCubes, blueCubes = 0, 0, 0
+        gameId = game.split(':')[0]
+        game = game.split(':')[1].strip()
+
+        gameRounds = game.split(';')
+
+        for gameRound in gameRounds:
+            for cube in gameRound.split(','):
+                cube = cube.strip()
+                color = cube.split(' ')[1]
+                count = int(cube.split(' ')[0])
+
+                if color == 'red':
+                    redCubes = max(redCubes, count)
+                elif color == 'green':
+                    greenCubes = max(greenCubes, count)
+                elif color == 'blue':
+                    blueCubes = max(blueCubes, count)
+       
+        power = redCubes * greenCubes * blueCubes
+
+        result += power
+    print(result)
